42640d6ffa85ce99d0a6c1890878d2321c5d6cda
Goreczky Roland <goreczky.roland@gmail.com>
2017-01-09 15:37:45 +0000

    code format

--- Step-5/application/script/app/extend/validation.js
+++ Step-5/application/script/app/extend/validation.js
@@ -1,12 +1,12 @@
 define([ 'handlebars' ],
   function( Handlebars ){
     return{
-			valid: function (view, attr, selector) {
-				var $el = view.$('[name=' + attr + ']'), 
-									$group = $el.closest('.form-group');    
-				$group.removeClass('has-error');
-				$group.find('.help-block').html('').addClass('hidden');
-			},
+      valid: function (view, attr, selector) {
+        var $el = view.$('[name=' + attr + ']'), 
+            $group = $el.closest('.form-group');    
+        $group.removeClass('has-error');
+        $group.find('.help-block').html('').addClass('hidden');
+      },
 			invalid: function (view, attr, error, selector) {
 				var $el = view.$('[name=' + attr + ']'),
 									$group = $el.closest('.form-group');
