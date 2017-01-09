9e1a2f84019e9d601e09efc43ada2fde16a0d014
Goreczky Roland <goreczky.roland@gmail.com>
2017-01-09 15:40:36 +0000

    code format

--- Step-4/application/script/app/helper/translate.js
+++ Step-4/application/script/app/helper/translate.js
@@ -1,5 +1,5 @@
 define([ 'handlebars' ],
-  function( Handlebars ){
+	function( Handlebars ){
 		Handlebars.registerHelper('t', function(i18n_key) {
 			if(!i18n_key) { return i18n_key; }
 			var result = App.i18next.t(i18n_key);
